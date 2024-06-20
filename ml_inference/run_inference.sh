83d9f16d05aecffb518d877056a3838768d4e159
StefanoS <s.simonetto@utwente.nl>
2024-06-20 17:49:58 +0200

    added run_iference.sh

---
+++ ml_inference/run_inference.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+python main_comparison/cve_all_terms/inference.py
+python main_comparison/cve_core/inference.py
+python main_comparison/cve_core_consequences/inference.py
+python main_comparison/cve_core_contextual/inference.py
+python main_comparison/cve_descr_repl_subj/inference.py
+python main_comparison/cve_description/inference.py
+
+python bert_phi_comparison/bert/inference.py
+python bert_phi_comparison/phi/inference.py
